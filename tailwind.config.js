f438af1a85e8dbaa605c26abdacd7cce6d4b8ce9
mikhailBorodastov <borodastoff@gmail.com>
2022-03-27 01:07:26 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'forest': '#84AEA4',
+      'swamp': '#62736F',
+      'backgr': '#F4F1E9',
+      'yellow': '#F2D97E',
+      'green': '#C7D6C1',
+      'white': '#F2F2F1',
+      'back': '#F4F1E9'
+    },
+    extend: {
+      fontFamily: {
+        main: "'San Francisco Pro Display Regular', sans-serif",
+        mainbold: "'San Francisco Pro Display Bold', sans-serif",
+        mainlight: "'San Francisco Pro Display Light', sans-serif",
+        haus: "'Lighthaus', mono"
+      }
+    },
+  },
+  plugins: [],
+}
